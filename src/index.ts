6c29c170bab16a6c3b92ecb5b1764757afc02a4b
radleylewis <sidwelllewis@myego2go.de>
2020-07-10 12:37:22 +0200

    chore: add TODOs

--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,13 @@ app.listen(port, err => {
   if (err) {
     return console.error(err);
   }
-  
   return console.log(`Server is listening on PORT:${port} 🚀`);
 });
+
+/* 
+TODO:
+0. set up .env (PARTNER_ID, API_KEY)
+1. Callback endpoint (user_id, contract_id)
+2. Read contract (incl. api_key)
+3. decrypt the contract (using private key)
+*/
