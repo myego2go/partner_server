9b8d0e3a88fb7b26ddd50e7f01200b338b5d8147
Msud0803 <manish.sudeshkumar@myego2go.de>
2020-07-15 10:22:29 +0200

    fix: added invokecallback controller

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,7 @@ const app = express();
 const port = 3088;
 app.use(express.json({ limit: "50mb" }));
 
-app.post('/', (req, res, next) => console.log(req.body, req.headers));
+app.post('/', invokeCallback);
 
 app.listen(port, err => {
   if (err) {
