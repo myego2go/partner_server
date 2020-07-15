6c4a3ea0b6b4723eef5502002f64649f5df49de1
Msud0803 <manish.sudeshkumar@myego2go.de>
2020-07-15 16:14:27 +0200

    fix: refactored code

--- src/controller.ts
+++ src/controller.ts
@@ -1,18 +1,42 @@
 import { Request, Response, NextFunction } from 'express';
 import get from 'request-promise';
+import fs from 'fs';
+import crypto from 'crypto';
 
 const api_url = process.env.EGO_API;
 const partner_id = process.env.PARTNER_ID;
 const api_key = process.env.API_KEY;
 
+const algorithm = 'aes-256-cbc';
+
+/* STEP 1: Read Private Key from file saved locally */
+const _privateKey = fs.readFileSync('private_key.ego', 'utf8');
+
+/* Private method for decrypting a specific contract */
+const _decryptPayload = ({ encryptedContract, encryptedAESKeyPartner, _privateKey }) => {
+  try {
+    const decryptedKey = crypto.privateDecrypt(_privateKey, Buffer.from(encryptedAESKeyPartner, "base64")).toString('utf8');
+    const [iv, encryptedText] = encryptedContract.split(':').map(part => Buffer.from(part, 'hex'));
+    const decipher = crypto.createDecipheriv(algorithm, Buffer.from(decryptedKey, 'hex'), iv);
+    let decrypted = decipher.update(encryptedText);
+    decrypted = Buffer.concat([decrypted, decipher.final()]);
+    return JSON.parse(decrypted.toString());
+  } catch (err) {
+    console.log('Decryption Error:', err.message);
+  }
+};
+
+/* receive contract_id and user_id and also decrypt Contract*/ 
 const invokeCallback = (req: Request, res: Response, next: NextFunction) => {
+
   console.log('POST: Callback URL has been contacted');
-  /* STEP: send OK on receipt of "user_id", "contract_id" */
+
+  /* STEP 2: send OK on receipt of "user_id", "contract_id" */
   const { user_id, contract_id } = req.body;
   if (user_id && contract_id) res.status(200).send();
   else return res.status(500).send();
   
-  /* STEP: destructure relevant variables from req.body and use them to retrieve the contract data */
+  /* STEP 3: destructure relevant variables from req.body and use them to retrieve the contract data */
   const options = {
     uri: `${api_url}/partner/api/${partner_id}/${user_id}/contracts/${contract_id}`,
     headers: {
@@ -21,42 +45,33 @@ const invokeCallback = (req: Request, res: Response, next: NextFunction) => {
     json: true
   };
 
-  /* STEP: call read contract endpoint from myEGO2GO API */
+  /* STEP 4: call read contract endpoint from myEGO2GO API */
   get(options)
     .then((res: Response) => {
       console.log('API: response received. Processing...');
-      console.log(res);
+      return res;
+    })
+
+    /* STEP 5: Exrtract encrypted payload from the response*/
+    .then(({_data}) => {
+      return _data.payload.payload;
+    })
+
+    /* STEP 6: Decrypt the Contract */
+    .then(({ encryptedContract, encryptedAESKeyPartner }) =>{
+      return _decryptPayload({ encryptedContract, encryptedAESKeyPartner, _privateKey });
+    })
+
+    /* STEP 7: receives decrypted contract */
+    .then((data) => {
+      /* This data can be used to connect database etc.. */
+      console.log("data:",data);  
     })
     .catch((err: Error) => {
       console.log(`ERROR: ${err.message}`);
-    });
-    
-    res.send();
+    }); 
 };
 
 export {
   invokeCallback
 };
-
-/*
-{
-  status: 0,
-  message: 'read_contract_success',
-  _data: {
-    clientId: '0fb78c06-618d-5ec9-be44-af89547c4796',
-    contractId: 'b0874a60-c5cf-11ea-8832-e5a0f791b2fc',
-    contract_version: 'x3o9iRsBBUcB92pn01UO_bazi67o7itC',
-    createDate: '2020-07-14T12:43:57.290Z',
-    partnerId: '3b1acc60-baf4-11ea-be50-cf2f41ea71f9',
-    payload: {
-      partner_id: '3b1acc60-baf4-11ea-be50-cf2f41ea71f9',
-      template_id: '0a3330e0-bf74-11ea-8da0-4b0fb5eb875c',
-      payload: [Object],
-      contractId: 'b0874a60-c5cf-11ea-8832-e5a0f791b2fc'
-    },
-    templateId: '0a3330e0-bf74-11ea-8da0-4b0fb5eb875c',
-    updateDate: '2020-07-14T12:43:57.290Z'
-  }
-}
-
-*/
