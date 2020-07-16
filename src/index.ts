c60a6aca63169b029ba923188f103218ee107416
Msud0803 <manish.sudeshkumar@myego2go.de>
2020-07-16 08:24:01 +0200

    fix: refactored code

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,6 @@
 import express from 'express';
-import dotenv from 'dotenv';
 
 import { invokeCallback } from './controller';
-
-dotenv.config();
 
 const app = express();
 const port = 3088;
@@ -17,11 +14,3 @@ app.listen(port, err => {
   }
   return console.log(`Server is listening on PORT:${port} 🚀`);
 });
-
-/* 
-TODO:
-0. set up .env (PARTNER_ID, API_KEY)
-1. Callback endpoint (user_id, contract_id)
-2. Read contract (incl. api_key)
-3. decrypt the contract (using private key)
-*/
